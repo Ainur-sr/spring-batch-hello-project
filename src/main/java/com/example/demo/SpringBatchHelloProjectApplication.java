eacbc0f44e378412fd643ff21bd8e97f69e740f0
Ainur-sr <ainshigapov@gmail.com>
2020-11-29 18:11:52 +0300

    initial commit

---
+++ src/main/java/com/example/demo/SpringBatchHelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchHelloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchHelloProjectApplication.class, args);
+	}
+
+}
